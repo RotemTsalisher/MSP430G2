160e69382ebb16125a69e7690fad2c0e35023381
Rotem Tsalisher <rotemt@cardosystems.com>
2025-06-15 23:29:04 +0300

    Blinking LED program w/ Macros

---
+++ Lab1P2S1Blink/main.c
@@ -0,0 +1,31 @@
+#include <msp430.h> 
+
+#define FALSE 0
+#define TRUE  !FALSE
+
+#define ISLOW(VECTOR, BIT)   (!(VECTOR & (0x01<<BIT)))
+#define TOGGLE(VECTOR, BIT)  (VECTOR ^= (0x01<<BIT))
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    int i;
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	P1OUT = 0x01<<3;
+	P1REN = 0x01<<3;
+	P1DIR = 0x01;
+	
+	while(TRUE)
+	{
+	    while(ISLOW(P1IN,3))
+	    {
+	        TOGGLE(P1OUT,0);
+	        for(i = 0; i<10000; i++);
+	    }
+	    P1OUT &= ~0x01;
+	}
+	return 0;
+}
